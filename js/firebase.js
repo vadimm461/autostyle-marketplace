99171af2d51799e09f9b360dd1053d568fe57809
vadimm461 <vadimm461@gmail.com>
2026-05-25 19:39:31 +0300

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -2,16 +2,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
-
 const firebaseConfig = {
   apiKey: 'PASTE_API_KEY',
   authDomain: 'auto-style-4dbb7.firebaseapp.com',
   projectId: 'auto-style-4dbb7',
-  storageBucket: 'auto-style-4dbb7.appspot.com',
+  storageBucket: 'auto-style-4dbb7.firebasestorage.app',
   messagingSenderId: '217023127803',
   appId: '1:217023127803:web:502ebd5d1981c8aeb0905e'
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
