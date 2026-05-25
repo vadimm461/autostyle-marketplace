fc1dc1384f6eeaa86e441cd319c151bfd7d5964d
vadimm461 <vadimm461@gmail.com>
2026-05-25 12:58:59 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
 
 const firebaseConfig = {
-  apiKey: 'PASTE_API_KEY',
-  authDomain: 'PASTE_PROJECT.firebaseapp.com',
-  projectId: 'PASTE_PROJECT_ID',
-  storageBucket: 'PASTE_PROJECT.appspot.com',
-  messagingSenderId: 'PASTE_SENDER_ID',
-  appId: 'PASTE_APP_ID'
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "auto-style-4dbb7.firebaseapp.com",
+  projectId: "auto-style-4dbb7",
+  storageBucket: "auto-style-4dbb7.appspot.com",
+  messagingSenderId: "217023127803",
+  appId: "1:217023127803:web:502ebd5d1981c8aeb0905e"
 };
 
 export const app = initializeApp(firebaseConfig);
