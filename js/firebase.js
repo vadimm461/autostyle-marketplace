b53e97f1e9245edbdbc0001749fe8168205d561d
vadimm461 <vadimm461@gmail.com>
2026-05-26 21:04:54 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,29 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBZ-AW6XoMR14KmBtlz2q06Z0jPGXnWMTw',
   authDomain: 'auto-style-4dbb7.firebaseapp.com',
   projectId: 'auto-style-4dbb7',
-  storageBucket: 'auto-style-4dbb7.appspot.com',
+
+  /* ВАЖНО */
+  storageBucket: 'auto-style-4dbb7.firebasestorage.app',
+
   messagingSenderId: '217023127803',
   appId: '1:217023127803:web:502ebd5d1981c8aeb0905e'
 };
 
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
 export const storage = getStorage(app);
 
 export const COLLECTIONS = {
